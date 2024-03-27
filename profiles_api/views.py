f4f3093618d1c309569faedf811b936d7e7cbfc2
udhavmore <udhav@djangoapp.com>
2024-03-27 22:33:53 +0530

    - Added create, retrieve, update, partial_update & destroy methods to ViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -54,6 +54,8 @@ class HelloViewSet(viewsets.ViewSet):
     def list(self, request):
         """Return a hello message"""
 
+        serializer_class = serializers.HelloSerializer
+
         a_viewset = [
             'Uses action (list, create, retrieve, update, partial_update)',
             'Automatically maps to URLs using Routers',
@@ -61,3 +63,31 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+    
+    def create(self, request):
+        """Create a new hello message"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get("name")
+            message = "Hello! {0}".format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        """Handles getting objects by its IDs"""
+        return Response({'http_method': 'GET'})
+    
+    def update(self, request, pk=None):
+        """Handels updating objects by its IDs"""
+        return Response({'http_method': 'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """Handles updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        """Handles removing an object"""
+        return Response({'http_method': 'DELETE'})
